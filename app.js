d949e93a434269b328d94bb9c7da4a3c4465d733
ethanfann <ecfann@gmail.com>
2020-09-17 18:16:50 -0700

    Use official json listing

--- app.js
+++ app.js
@@ -1,42 +1,24 @@
-const cheerio = require('cheerio');
+const axios = require('axios')
 const download = require('image-downloader')
-const fs = require('fs');
-const request = require("request-promise");
-const performance = require('perf_hooks').performance;
+const fs = require('fs')
 const Promise = require('bluebird')
 
-request({
-    url: 'https://slackmojis.com/',
-    json: false
-}).then(body => {
-    const $ = cheerio.load(body);
+axios.get('https://slackmojis.com/emojis.json').then((response) => {
+	const downloadList = response.data.map((emoji) => ({
+		url: emoji['image_url'],
+		dest: `emojis/${emoji['category'].name}`,
+	}))
 
-    let emojis = []
-    $('ul.groups').find('li.group').each(function(index){
-        let folder = $( this ).find('div.title').text().trim().replace(/\./g,' ')
-
-        $( this ).find('ul.emojis').children('li').each(function(index){
-            let url = $( this ).find('a').find('img').attr('src').split('?')[0]
-            emojis.push({
-                url: url,
-                dest: "emojis/" + folder
-            })
-        })
-    })
-
-    let start = performance.now();
-    if (!fs.existsSync('emojis')) fs.mkdirSync('emojis')
-    return Promise.mapSeries(emojis, emoji => {
-        if (!fs.existsSync(emoji.dest)) fs.mkdirSync(emoji.dest)
-        return download.image(emoji)
-            .then(({ filename }) => {
-                console.log('File saved to', filename)
-            })
-            .catch((err) => {
-                console.error(err)
-            })
-    }).finally(function() {
-        let end = performance.now();
-        console.log("Downloaded " + emojis.length + " emojis in " + ( (end-start) / 1000).toFixed(2) + " seconds.")
-    })    
+	if (!fs.existsSync('emojis')) fs.mkdirSync('emojis')
+	return Promise.mapSeries(downloadList, (emoji) => {
+		if (!fs.existsSync(emoji.dest)) fs.mkdirSync(emoji.dest)
+		return download
+			.image(emoji)
+			.then(({ filename }) => {
+				console.log('File saved to', filename)
+			})
+			.catch((err) => {
+				console.error(err)
+			})
+	})
 })
