8ca23aff9ce4c81a8e8f662371bdcaa0f5c07bbb
Ethan Fann <git@ethanfann.com>
2021-09-10 12:37:55 -0700

    Enhanced testing

--- test.js
+++ test.js
@@ -1,13 +1,49 @@
 const test = require('ava')
+const fs = require('fs')
+const path = require('path')
 const extractEmojiName = require('./util/extractEmojiName')
 const getLastPage = require('./util/getLastPage')
+const download = require('./util/download')
+const getPage = require('./util/getPage')
+const obtain = require('./util/obtain')
+const prepare = require('./util/prepare')
+const loadExistingEmojis = require('./util/loadExistingEmojis')
 
-test('parse a url to obtain an emoji name', (t) => {
-  const name =
-    'https://emojis.slackmojis.com/emojis/images/1615690644/20375/0.gif?1615690644'
-  const extracted = extractEmojiName(name)
+const testDir = `${__dirname}/test/emojis`
+fs.rmdirSync(testDir, { recursive: true })
+fs.mkdirSync(testDir, { recursive: true })
 
-  t.is(extracted, '0.gif')
+test('downloads emojis', async (t) => {
+  const results = await getPage(0)
+  const prepared = prepare(results, null, testDir)
+
+  const emoji = prepared[0]
+  if (!fs.existsSync(emoji.dest)) fs.mkdirSync(emoji.dest)
+  await download(emoji.url, path.join(emoji.dest, emoji.name))
+
+  t.is(loadExistingEmojis(testDir).length === 1, true)
+})
+
+test('filter emojis when a category is specified', async (t) => {
+  const results = await getPage(0)
+  const prepared = prepare(results, 'Party Parrot', testDir)
+
+  let flag = false
+  prepared.forEach((emoji) => (flag = emoji.dest.includes('Party Parrot')))
+
+  t.is(prepared.length < results.length && flag, true)
+})
+
+test('obtains single pages of emojis', async (t) => {
+  const results = await getPage(0)
+
+  t.is(results.length > 0, true)
+})
+
+test('obtains multiple pages of emojis', async (t) => {
+  const results = await obtain(2, 3)
+
+  t.is(results.length === 1000, true)
 })
 
 test('correctly obtain a given last page of 67', async (t) => {
@@ -26,4 +62,12 @@ test('correctly obtain a given last page of 139', async (t) => {
   const page = await getLastPage(true, 139)
 
   t.is(page, 139)
+})
+
+test('parse a url to obtain an emoji name', (t) => {
+  const name =
+    'https://emojis.slackmojis.com/emojis/images/1615690644/20375/0.gif?1615690644'
+  const extracted = extractEmojiName(name)
+
+  t.is(extracted, '0.gif')
 })
